35da78c6b6c39405dd6817dbad299e7f7453ceb5
wangle612 <79564854+wangle612@users.noreply.github.com>
2026-04-01 18:32:05 +0800

    Update app.tsx

--- client/src/app.tsx
+++ client/src/app.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Route, Routes } from 'react-router-dom';
+import { Route, Routes, HashRouter } from 'react-router-dom';
 
 import Layout from './components/Layout';
 import NotFound from './pages/NotFound/NotFound';
@@ -9,14 +9,16 @@ import OrdersPage from './pages/OrdersPage/OrdersPage';
 
 const RoutesComponent = () => {
   return (
-    <Routes>
-      <Route element={<Layout />}>
-        <Route index element={<CashierPage />} />
-        <Route path="products" element={<ProductsPage />} />
-        <Route path="orders" element={<OrdersPage />} />
-      </Route>
-      <Route path="*" element={<NotFound />} />
-    </Routes>
+    <HashRouter>
+      <Routes>
+        <Route element={<Layout />}>
+          <Route index element={<CashierPage />} />
+          <Route path="products" element={<ProductsPage />} />
+          <Route path="orders" element={<OrdersPage />} />
+        </Route>
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </HashRouter>
   );
 };
 
